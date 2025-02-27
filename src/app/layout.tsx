2098234cb30e81bd4705fff13bc779e828688fc2
NikoSAN02 <dineshbohra@gmail.com>
2025-02-28 03:45:02 +0530

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,15 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Providers from "./Providers";
 import Script from "next/script";
+import ConditionalLayout from "@/components/ConditionalLayout";
+import React,{ Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "thirdweb SDK + Next starter",
+  title: "BTFD Web",
   description:
-    "Starter template for using thirdweb SDK with Next.js App router",
+    "BTFD  Web",
 };
 
 export default function RootLayout({
@@ -18,12 +20,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>
-          {children}
-        </Providers>
-        <Script src="https://telegram.org/js/telegram-web-app.js" />
+    <html lang="en" style={{ overflow: "auto", height: "100%" }}>
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+      <body
+        className={inter.className}
+        style={{ overflow: "auto", height: "100%", margin: 0 }}
+      >
+         <Suspense fallback={
+          <div className="h-screen bg-black w-full flex items-center justify-center">
+         <div className="font-bold font-zk text-white">Please wait...</div>
+          </div>
+         }>
+        <ConditionalLayout>
+        <Providers>{children}</Providers></ConditionalLayout>
+        </Suspense>
       </body>
     </html>
   );
---
+++ src/components/ConditionalLayout.tsx
@@ -0,0 +1,31 @@
+// ConditionalLayout.tsx
+'use client'
+
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+const ConditionalLayout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+  const [isUnityRoute, setIsUnityRoute] = useState(false);
+
+  useEffect(() => {
+    setIsUnityRoute(pathname.startsWith('/unity'));
+    console.log('Current pathname:', pathname);
+    console.log('Is Unity route:', pathname.startsWith('/unity'));
+  }, [pathname]);
+
+  if (typeof window !== 'undefined') {
+    console.log('Window location:', window.location.pathname);
+  }
+
+  return (
+    <>
+
+      {children}
+      {!isUnityRoute}
+     
+    </>
+  );
+};
+
+export default ConditionalLayout;
